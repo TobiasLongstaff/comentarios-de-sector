9646c670ca4a513c8cc123fa2fc22c2a94b56c76
tlongstaff <tobilongstaff@outlook.com>
2022-09-05 15:30:12 -0300

    v1.0.12

--- partials/conexion.php
+++ partials/conexion.php
@@ -1,11 +1,11 @@
 <?php
 
-    $conexion = mysqli_connect('localhost', 'root', '', 'comentarios-de-sector');
+    $conexion = mysqli_connect('localhost', 'root', '', 'c2271018_coment');
     if (mysqli_connect_errno())
     {
         $_SESSION['message-error'] = 'Error al conectar la base de datos';
         exit();
     }
-    mysqli_select_db($conexion, 'comentarios-de-sector') or die ($_SESSION['message-error'] = 'Error al conectar');
+    mysqli_select_db($conexion, 'c2271018_coment') or die ($_SESSION['message-error'] = 'Error al conectar');
     mysqli_set_charset($conexion, 'utf8');
 ?>
